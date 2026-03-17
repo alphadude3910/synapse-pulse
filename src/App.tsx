cd68d6bd342d3b9b6a243e066ef533367bda90e1
alphadude3910 <rohansingh469062@gmail.com>
2026-03-17 18:58:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
+import DiscoverPage from "./pages/Discover";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
